25a7db19e523faa5eec36218f227f2d83fe8916e
anhhuynhnhut <huynhnhutanh2000@gmail.com>
2024-05-23 16:09:34 +0700

    a

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,3 @@
-const cacheName = "DefaultCompany-VirtualTour-0.1";
-const contentToCache = [
-    "Build/VT-demo.loader.js",
-    "Build/VT-demo.framework.js",
-    "Build/VT-demo.data",
-    "Build/VT-demo.wasm",
-    "TemplateData/style.css"
-
-];
-
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
-    
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
-});
-
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:f562f57a288a73501573102f908967d6f75e4ab9badf3717735a71e79246c3bd
+size 1052
